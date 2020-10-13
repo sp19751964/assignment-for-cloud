9a9587ac912a0dafbfe2204c4b135c8f5386ce18
sp19751964 <54701429+sp19751964@users.noreply.github.com>
2020-10-13 17:49:12 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
-import calendar
+import calendar  
 
-yy = 2014  
-mm = 11    
+yy = int(input("Enter year: "))  
+mm = int(input("Enter month: "))  
+  
 
-
-print(calendar.month(yy, mm))
+print(calendar.month(yy,mm))  
